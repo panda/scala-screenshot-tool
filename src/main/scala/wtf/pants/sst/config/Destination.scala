a8ed8cffa0879ae53fc20195372429fbfa5cd095
Pants <Pants@users.noreply.github.com>
2018-03-17 23:30:31 -0400

    Formatting

--- src/main/scala/wtf/pants/sst/config/Destination.scala
+++ src/main/scala/wtf/pants/sst/config/Destination.scala
@@ -6,8 +6,8 @@ import wtf.pants.sst.config.enum.RequestType.RequestType
 
 class Destination(val url: String,
                   val fileField: String,
-                                  var arguments: Array[(String, String)] = new Array[(String, String)](0),
-                                  val requestType: RequestType = RequestType.POST) {
+                  var arguments: Array[(String, String)] = new Array[(String, String)](0),
+                  val requestType: RequestType = RequestType.POST) {
 
   def updateArgs(newArgs: String): Unit = {
     arguments = newArgs.split("&").map(s => (s.split("=")(0), s.split("=")(1)))
